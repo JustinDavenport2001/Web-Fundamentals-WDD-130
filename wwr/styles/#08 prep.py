94a044e142202cf0c135e97524ee7fca185b8b94
JustinDavenport2001 <davenpojustin@gmail.com>
2024-11-08 16:53:38 -0700

    Wirey

---
+++ wwr/styles/#08 prep.py
@@ -0,0 +1,16 @@
+#08 prep
+
+colors = ["red", "blue", "green", "yellow"]
+
+for color in colors:
+    print(color)
+    
+print()
+
+for i in range(1, 9):
+    print(i)
+    
+print()
+
+for b in range(2, 21, 2):
+    print(b)
