eacf86ac89debb50f9ffce18b35044489aacb5db
JustinDavenport2001 <davenpojustin@gmail.com>
2024-11-26 17:08:07 -0700

    site plan in footer

--- practice/#W04 Prepare.py
+++
@@ -1,4 +0,0 @@
-#W04 Prepare
-degrees_f = float(input('What is the temperature in Fahrenheit? '))
-degrees_c = (degrees_f - 32) * 5 / 9
-print(f'The temperature in Celsius is {degrees_c: .1f} degrees')
